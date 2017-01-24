ca85e15a1ae28f4b40245b4bc7b572f4ed6db773
Åke Engelbrektson <eson57@users.noreply.github.com>
2017-01-24 07:19:06 +0100

    Create harbour-mmslog-sv.ts

---
+++ translations/harbour-mmslog-sv.ts
@@ -0,0 +1,91 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name></name>
+    <message id="mmslog-cover-title">
+        <source>MMS</source>
+        <translation>MMS</translation>
+    </message>
+    <message id="mmslog-logpage-pm-pack-and-send">
+        <source>Pack and send</source>
+        <translation>Paketera och sänd</translation>
+    </message>
+    <message id="mmslog-logpage-pm-clear-log">
+        <source>Clear log</source>
+        <translation>Rensa loggen</translation>
+    </message>
+    <message id="mmslog-logpage-title">
+        <source>MMS engine log</source>
+        <translation>MMS-motorlogg</translation>
+    </message>
+    <message id="mmslog-sharemethodlist-add-account">
+        <source>Add account</source>
+        <translation>Lägg till konto</translation>
+    </message>
+    <message id="mmslog-sharepage-header">
+        <source>Pack and send</source>
+        <translation>Paketera och sänd</translation>
+    </message>
+    <message id="mmslog-sharepage-warning">
+        <source>Keep in mind that some of the information contained in this archive may be considered private. If you would like to check what you are about to send, please consider sending it to yourself first and emailing this file to mms-debug@jolla.com later from your computer. If you trust Jolla, then you can conveniently email it to Jolla directly from your phone.</source>
+        <translation>Notera att viss information i detta arkiv kan anses vara av privat natur. Om du vill veta vad du är på väg att skicka, kan du skicka det till dig själv först och sedan skicka filen vidare till mms-debug@jolla.com, från din dator. Om du litar på Jolla, kan du naturligtvis skicka den direkt till Jolla från din telefon.</translation>
+    </message>
+    <message id="mmslog-sharepage-please-wait">
+        <source>Please wait</source>
+        <translation>Vänta</translation>
+    </message>
+    <message id="mmslog-sharepage-pm-save-to-documents">
+        <source>Save to documents</source>
+        <translation>Spara i Dokument</translation>
+    </message>
+    <message id="mmslog-sharepage-save-ok">
+        <source>Saved %1</source>
+        <translation>Sparade %1</translation>
+    </message>
+    <message id="mmslog-sharepage-save-error">
+        <source>Failed to save %1</source>
+        <translation>Kunde inte spara %1</translation>
+    </message>
+    <message id="mmslog-settings-page-header">
+        <source>MMS Logger</source>
+        <translation>MMS Logger</translation>
+    </message>
+    <message id="mmslog-settings-logsizelimit">
+        <source>Screen buffer size</source>
+        <translation>Skärmbuffertstorlek</translation>
+    </message>
+    <message id="mmslog-settings-logsizelimit-description">
+        <source>Don&apos;t worry, everything will be written to the log file regardless of the screen buffer size.</source>
+        <translation>Oroa dig inte, allt kommer att skrivas till loggfilen oavsett skärmbuffertstorlek.</translation>
+    </message>
+    <message id="mmslog-settings-logsizelimit-unlimited">
+        <source>Unlimited</source>
+        <translation>Obegränsad</translation>
+    </message>
+    <message id="mmslog-settings-fontsize-label">
+        <source>Font size</source>
+        <translation>Teckenstorlek</translation>
+    </message>
+    <message id="mmslog-settings-ofonolog-label">
+        <source>Ofono log</source>
+        <translation>Ofono-logg</translation>
+    </message>
+    <message id="mmslog-settings-ofonolog-description">
+        <source>Ofono log provides additional information on what&apos;s happening at telephony level. Currently this log is not visible in the user interface but if enabled, it&apos;s packed and stored in the tarball.</source>
+        <translation>Ofono-loggen tillhandahåller mer information om vad som händer på telefonnivå. För närvarande är denna logg inte synlig i användargränssnittet, men vid aktivering paketeras den och lagras i samma paket.</translation>
+    </message>
+    <message id="mmslog-settings-ofonolog-value-off">
+        <source>Off</source>
+        <translation>Av</translation>
+    </message>
+    <message id="mmslog-settings-ofonolog-value-normal">
+        <source>Normal</source>
+        <translation>Normal</translation>
+    </message>
+    <message id="mmslog-settings-ofonolog-value-full">
+        <source>Full</source>
+        <translation>Fullständig</translation>
+    </message>
+</context>
+</TS>
