a292bdbd8b9f441652e91b325c3777206f36da9c
d9h20f <>
2018-01-30 15:02:32 +0100

    Dutch translation

---
+++ translations/harbour-mmslog-nl.ts
@@ -0,0 +1,98 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl">
+<context>
+    <name></name>
+    <message id="mmslog-cover-title">
+        <source>MMS</source>
+        <translation>Mms</translation>
+    </message>
+    <message id="mmslog-logpage-pm-settings">
+        <source>Settings</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Instellingen</translation>
+    </message>
+    <message id="mmslog-logpage-pm-pack-and-send">
+        <source>Pack and send</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Inpakken en verzenden</translation>
+    </message>
+    <message id="mmslog-logpage-pm-clear-log">
+        <source>Clear log</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Log wissen</translation>
+    </message>
+    <message id="mmslog-logpage-title">
+        <source>MMS engine log</source>
+        <translation>Mms-enginelog</translation>
+    </message>
+    <message id="mmslog-sharemethodlist-add-account">
+        <source>Add account</source>
+        <translation>Account toevoegen</translation>
+    </message>
+    <message id="mmslog-sharepage-header">
+        <source>Pack and send</source>
+        <translation>Inpakken en verzenden</translation>
+    </message>
+    <message id="mmslog-sharepage-warning">
+        <source>Keep in mind that some of the information contained in this archive may be considered private. If you would like to check what you are about to send, please consider sending it to yourself first and emailing this file to mms-debug@jolla.com later from your computer. If you trust Jolla, then you can conveniently email it to Jolla directly from your phone.</source>
+        <translation>Let op: sommige informatie in dit archief is mogelijk privé. Als je wil nakijken wat je gaat verzenden, kan je het eerst naar je eigen e-mailadres verzenden en daar de inhoud controleren, en daarna vanop je computer dit bestand naar mms-debug@jolla.com te verzenden. Als je Jolla vertrouwt, kan je het gewoonweg rechtstreeks vanop je telefoon e-mailen.</translation>
+    </message>
+    <message id="mmslog-sharepage-please-wait">
+        <source>Please wait</source>
+        <translation>Even geduld</translation>
+    </message>
+    <message id="mmslog-sharepage-pm-save-to-documents">
+        <source>Save to documents</source>
+        <translation>Opslaan naar documenten</translation>
+    </message>
+    <message id="mmslog-sharepage-save-ok">
+        <source>Saved %1</source>
+        <translation>%1 opgeslagen</translation>
+    </message>
+    <message id="mmslog-sharepage-save-error">
+        <source>Failed to save %1</source>
+        <translation>Opslaan van %1 mislukt</translation>
+    </message>
+    <message id="mmslog-settings-page-header">
+        <source>MMS Logger</source>
+        <translation>Mms-logger</translation>
+    </message>
+    <message id="mmslog-settings-logsizelimit">
+        <source>Screen buffer size</source>
+        <translation>Schermbuffergrootte</translation>
+    </message>
+    <message id="mmslog-settings-logsizelimit-description">
+        <source>Don&apos;t worry, everything will be written to the log file regardless of the screen buffer size.</source>
+        <translation>Geen zorgen, alles wordt naar het logbestand geschreven, ongeacht de schermbuffergrootte.</translation>
+    </message>
+    <message id="mmslog-settings-logsizelimit-unlimited">
+        <source>Unlimited</source>
+        <translation>Onbeperkt</translation>
+    </message>
+    <message id="mmslog-settings-fontsize-label">
+        <source>Font size</source>
+        <translation>Lettergrootte</translation>
+    </message>
+    <message id="mmslog-settings-ofonolog-label">
+        <source>Ofono log</source>
+        <translation>Ofonoølog</translation>
+    </message>
+    <message id="mmslog-settings-ofonolog-description">
+        <source>Ofono log provides additional information on what&apos;s happening at telephony level. Currently this log is not visible in the user interface but if enabled, it&apos;s packed and stored in the tarball.</source>
+        <translation>Het ofono-log biedt extra informatie over wat er op telefonieniveau gebeurt. Dit log is momenteel niet zichtbaar in de gebruikersinterface, maar indien ingeschakeld wordt het ingepakt en opgeslagen in de tarbal.</translation>
+    </message>
+    <message id="mmslog-settings-ofonolog-value-off">
+        <source>Off</source>
+        <translation>Uit</translation>
+    </message>
+    <message id="mmslog-settings-ofonolog-value-normal">
+        <source>Normal</source>
+        <translation>Normaal</translation>
+    </message>
+    <message id="mmslog-settings-ofonolog-value-full">
+        <source>Full</source>
+        <translation>Volledig</translation>
+    </message>
+</context>
+</TS>
