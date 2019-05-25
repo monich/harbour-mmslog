651de9751d532fd4ba19a25cf30495edae054a33
Slava Monich <slava.monich@jolla.com>
2019-05-25 13:04:44 +0300

    Use a different mechanism of killing mms-engine
    The previously used mechanism of killing (or rather, finding) mms-engine
    stopped working in Sailfish OS 3.0.3.

--- src/mmsengine.cpp
+++ src/mmsengine.cpp
@@ -1,37 +1,39 @@
 /*
-  Copyright (C) 2014-2018 Jolla Ltd.
-  Copyright (C) 2014-2018 Slava Monich <slava.monich@jolla.com>
-
-  You may use this file under the terms of BSD license as follows:
-
-  Redistribution and use in source and binary forms, with or without
-  modification, are permitted provided that the following conditions
-  are met:
-
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name of the Jolla Ltd nor the
-      names of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written permission.
-
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS
-  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
-  THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ * Copyright (C) 2014-2019 Jolla Ltd.
+ * Copyright (C) 2014-2019 Slava Monich <slava.monich@jolla.com>
+ *
+ * You may use this file under the terms of BSD license as follows:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   1. Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *   3. Neither the names of the copyright holders nor the names of its
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include "mmsengine.h"
 #include "mmsenginelog.h"
+
+#include "dbusaccess_peer.h"
 
 #include "HarbourDebug.h"
 
@@ -44,6 +46,7 @@
 #include <sys/prctl.h>
 
 #define MMS_ENGINE "/usr/sbin/mms-engine"
+#define MMS_ENGINE_NAME "org.nemomobile.MmsEngine"
 #define MMS_ENGINE_RESTART_DELAY (1000)
 #define MMS_ENGINE_MAX_RESTARTS  (10)
 
@@ -85,17 +88,11 @@ void MMSEngine::stopLogging()
 
 void MMSEngine::killEngine()
 {
-    QDir proc("/proc");
-    QStringList entries = proc.entryList(QDir::Dirs | QDir::NoDotAndDotDot);
-    foreach (QString entry, entries) {
-        int pid = entry.toInt();
-        if (pid) {
-            QString exe = QFile::symLinkTarget(proc.filePath(entry).append("/exe"));
-            if (!exe.isEmpty() && QFile::exists(exe) && exe == MMS_ENGINE) {
-                HDEBUG("MMS engine already running, pid" << pid);
-                kill(pid, SIGTERM);
-                break;
-            }
+    DAPeer* peer = da_peer_get(DA_BUS_SYSTEM, MMS_ENGINE_NAME);
+    if (peer) {
+        HDEBUG("MMS engine already running, pid" << peer->pid);
+        if (kill(peer->pid, SIGTERM) < 0) {
+            HWARN("Failed to kill MMS engine:" << strerror(errno));
         }
     }
 }
@@ -115,7 +112,7 @@ MMSEngineLog* MMSEngine::startEngine()
             iPid = fork();
             if (iPid > 0) {
                 // Parent
-                HDEBUG("Stared MMS engine, pid" << iPid);
+                HDEBUG("Started MMS engine, pid" << iPid);
                 return startLogThread(iPipe = pty);
             } else if (iPid == 0) {
                 // Child
